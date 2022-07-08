49204ce21d11028366efb0fff04f16db4a184bb9
ankitchy123 <ankitchaudhary.ac9611@gmail.com>
2022-07-08 12:38:05 +0530

    String added

---
+++ String/1108. Defanging an IP Address.cpp
@@ -0,0 +1,20 @@
+class Solution
+{
+public:
+    string defangIPaddr(string address)
+    {
+        string str = "";
+        for (int i = 0; i < address.size(); i++)
+        {
+            if (address[i] == '.')
+            {
+                str += "[.]";
+                continue;
+            }
+
+            str += address[i];
+        }
+
+        return str;
+    }
+};
