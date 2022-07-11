ab3335701a06a8cfe490bde31377636dd964b223
ankitchy123 <ankitchaudhary.ac9611@gmail.com>
2022-07-12 02:18:05 +0530

    12-07-22

---
+++ String/1221. Split a String in Balanced Strings.cpp
@@ -0,0 +1,28 @@
+class Solution
+{
+public:
+    int balancedStringSplit(string s)
+    {
+        int ans = 0;
+        int count = 0;
+
+        for (int i = 0; i < s.size(); i++)
+        {
+            if (s[i] == 'L')
+            {
+                count++;
+            }
+            else if (s[i] == 'R')
+            {
+                count--;
+            }
+
+            if (count == 0)
+            {
+                ans++;
+            }
+        }
+
+        return ans;
+    }
+};
