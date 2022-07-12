2f23a2818f9588d5e501fd68644251b60ea46876
ankitchy123 <ankitchaudhary.ac9611@gmail.com>
2022-07-13 00:51:11 +0530

    13-07-22

---
+++ Array/2037. Minimum Number of Moves to Seat Everyone.cpp
@@ -0,0 +1,17 @@
+class Solution
+{
+public:
+    int minMovesToSeat(vector<int> &seats, vector<int> &students)
+    {
+        int moves = 0;
+        sort(seats.begin(), seats.end());
+        sort(students.begin(), students.end());
+
+        for (int i = 0; i < seats.size(); i++)
+        {
+            moves += abs(students[i] - seats[i]);
+        }
+
+        return moves;
+    }
+};
