b98512464e017d1387dd29ff8c68ee0686797916
ankitchy123 <ankitchaudhary.ac9611@gmail.com>
2022-07-21 21:23:57 +0530

    21-07-22

---
+++ String/14. Longest Common Prefix.cpp
@@ -0,0 +1,29 @@
+class Solution
+{
+public:
+    string longestCommonPrefix(vector<string> &strs)
+    {
+        if (strs.size() == 0)
+        {
+            return "";
+        }
+        string ans = "";
+        sort(begin(strs), end(strs));
+        string a = strs[0];
+        string b = strs[strs.size() - 1];
+
+        for (int i = 0; i < a.size(); i++)
+        {
+            if (a[i] == b[i])
+            {
+                ans += a[i];
+            }
+            else
+            {
+                break;
+            }
+        }
+
+        return ans;
+    }
+};
