1ace963ce524265f26cb980440763ed9158a335d
ankitchy123 <ankitchaudhary.ac9611@gmail.com>
2022-07-21 22:08:11 +0530

    21-07-22

---
+++ Stack/20. Valid Parentheses.cpp
@@ -0,0 +1,58 @@
+class Solution
+{
+public:
+    bool isValid(string s)
+    {
+        stack<int> st;
+        bool ans = true;
+
+        for (int i = 0; i < s.size(); i++)
+        {
+            if (s[i] == '(' || s[i] == '{' || s[i] == '[')
+            {
+                st.push(s[i]);
+            }
+            else if (s[i] == ')')
+            {
+                if (!st.empty() && st.top() == '(')
+                {
+                    st.pop();
+                }
+                else
+                {
+                    ans = false;
+                    break;
+                }
+            }
+            else if (s[i] == ']')
+            {
+                if (!st.empty() && st.top() == '[')
+                {
+                    st.pop();
+                }
+                else
+                {
+                    ans = false;
+                    break;
+                }
+            }
+            else if (s[i] == '}')
+            {
+                if (!st.empty() && st.top() == '{')
+                {
+                    st.pop();
+                }
+                else
+                {
+                    ans = false;
+                    break;
+                }
+            }
+        }
+        if (!st.empty())
+        {
+            ans = false;
+        }
+        return ans;
+    }
+};
