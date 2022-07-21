3451462d045839cb87a559d3724073855a2aefad
ankitchy123 <ankitchaudhary.ac9611@gmail.com>
2022-07-21 21:15:56 +0530

    21-07-22

---
+++ String/58. Length of Last Word.cpp
@@ -0,0 +1,22 @@
+class Solution
+{
+public:
+    int lengthOfLastWord(string s)
+    {
+        int ans = 0;
+        for (int i = s.size() - 1; i >= 0; i--)
+        {
+            if (s[i] == ' ')
+            {
+                if (ans == 0)
+                {
+                    continue;
+                }
+                break;
+            }
+            ans++;
+        }
+
+        return ans;
+    }
+};
