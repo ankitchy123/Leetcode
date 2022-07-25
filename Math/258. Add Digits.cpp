4cf9dadc1fbfca10808b9647a793975d85db43c8
ankitchy123 <ankitchaudhary.ac9611@gmail.com>
2022-07-26 02:30:51 +0530

    26-07-22

---
+++ Math/258. Add Digits.cpp
@@ -0,0 +1,24 @@
+class Solution
+{
+public:
+    int addDigits(int num)
+    {
+        if (num < 10)
+        {
+            return num;
+        }
+        int ans = 0;
+        while (num >= 10)
+        {
+            ans = 0;
+            while (num)
+            {
+                ans += num % 10;
+                num /= 10;
+            }
+            num = ans;
+        }
+
+        return ans;
+    }
+};
