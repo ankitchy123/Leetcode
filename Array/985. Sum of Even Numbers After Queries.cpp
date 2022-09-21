e0bbac600659d1014c52afe5083748069c3e0e4c
ankitchy123 <ankitchaudhary.ac9611@gmail.com>
2022-09-21 21:50:05 +0530

    21-09-22

---
+++ Array/985. Sum of Even Numbers After Queries.cpp
@@ -0,0 +1,33 @@
+class Solution
+{
+public:
+    vector<int> sumEvenAfterQueries(vector<int> &nums, vector<vector<int>> &queries)
+    {
+        vector<int> vec;
+        int sum = 0;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (nums[i] % 2 == 0)
+            {
+                sum += nums[i];
+            }
+        }
+
+        for (int i = 0; i < queries.size(); i++)
+        {
+            if (nums[queries[i][1]] % 2 == 0)
+            {
+                sum -= nums[queries[i][1]];
+            }
+
+            nums[queries[i][1]] += queries[i][0];
+            if (nums[queries[i][1]] % 2 == 0)
+            {
+                sum += nums[queries[i][1]];
+            }
+            vec.push_back(sum);
+            cout << sum << " ";
+        }
+        return vec;
+    }
+};
