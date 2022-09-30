909789957c1f6743e2befe5d9baeda91f8e88886
ankitchy123 <ankitchaudhary.ac9611@gmail.com>
2022-09-30 20:58:46 +0530

    30-09-22

---
+++ Greedy/1833. Maximum Ice Cream Bars.cpp
@@ -0,0 +1,16 @@
+class Solution
+{
+public:
+    int maxIceCream(vector<int> &costs, int coins)
+    {
+        sort(costs.begin(), costs.end());
+
+        int i = 0;
+        while (i < costs.size() && coins - costs[i] >= 0)
+        {
+            coins = coins - costs[i];
+            i++;
+        }
+        return i;
+    }
+};
