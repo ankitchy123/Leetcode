32cdcfe9aa6fafe5b1357566101ccf31e0dde65e
ankitchy123 <ankitchaudhary.ac9611@gmail.com>
2022-11-07 20:41:34 +0530

    07-11-22

--- Math/1323. Maximum 69 Number.cpp
+++ Math/1323. Maximum 69 Number.cpp
@@ -17,3 +17,27 @@ public:
         return stoi(s);
     }
 };
+
+// OR
+
+class Solution
+{
+public:
+    int maximum69Number(int num)
+    {
+        int n = num;
+        int indexFirstSix = -1;
+        int ind = 0;
+
+        while (n > 0)
+        {
+            if (n % 10 == 6)
+            {
+                indexFirstSix = ind;
+            }
+            n /= 10;
+            ind++;
+        }
+        return indexFirstSix == -1 ? num : num + 3 * (int)pow(10, indexFirstSix);
+    }
+};
