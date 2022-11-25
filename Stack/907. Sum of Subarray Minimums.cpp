334cbca9588dd870e3b09abb3e26d202248fc3de
ankitchy123 <ankitchaudhary.ac9611@gmail.com>
2022-11-26 00:25:26 +0530

    26-11-22

---
+++ Stack/907. Sum of Subarray Minimums.cpp
@@ -0,0 +1,23 @@
+class Solution
+{
+public:
+    int sumSubarrayMins(vector<int> &arr)
+    {
+        stack<int> st;
+        long ans = 0;
+        for (int i = 0; i <= arr.size(); i++)
+        {
+            while (!st.empty() && (i == arr.size() || arr[st.top()] > arr[i]))
+            {
+                int ind = st.top();
+                st.pop();
+                int left = ind - (st.empty() ? -1 : st.top());
+                int right = i - ind;
+
+                ans += (long)arr[ind] * left * right;
+            }
+            st.push(i);
+        }
+        return ans % 1000000007;
+    }
+};
