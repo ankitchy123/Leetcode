c541fc1d6a2bc5551fa7c564dc9eaaa8f570c789
ankitchy123 <ankitchaudhary.ac9611@gmail.com>
2023-02-13 09:57:29 +0530

    13-02-23

---
+++ Math/1523. Count Odd Numbers in an Interval Range.cpp
@@ -0,0 +1,13 @@
+class Solution
+{
+public:
+    int countOdds(int low, int high)
+    {
+        int ans = (high - low + 1) / 2 + (high - low + 1) % 2;
+        if (low % 2 == 0 && high % 2 == 0)
+        {
+            ans--;
+        }
+        return ans;
+    }
+};
