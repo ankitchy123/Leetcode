55926b5466d3ff4ec3959a9f1a49dac00ab0909c
ankitchy123 <ankitchaudhary.ac9611@gmail.com>
2023-02-23 23:24:35 +0530

    23-02-23

---
+++ Queue/Priority Queue/502. IPO.cpp
@@ -0,0 +1,31 @@
+class Solution
+{
+public:
+    int findMaximizedCapital(int k, int w, vector<int> &profits, vector<int> &capital)
+    {
+        int n = profits.size();
+        vector<pair<int, int>> vec;
+        for (int i = 0; i < n; i++)
+        {
+            vec.push_back({capital[i], profits[i]});
+        }
+        sort(vec.begin(), vec.end());
+        priority_queue<int> pq;
+        int j = 0;
+        for (int i = 0; i < k; i++)
+        {
+            while (j < n && vec[j].first <= w)
+            {
+                pq.push(vec[j].second);
+                j++;
+            }
+            if (pq.empty())
+            {
+                break;
+            }
+            w += pq.top();
+            pq.pop();
+        }
+        return w;
+    }
+};
